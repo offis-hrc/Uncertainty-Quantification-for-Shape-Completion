9f874eb2527f5db4fc54187b66bb69953886f33a
“Pranav <pranav.megarajan@offis.de>
2024-10-25 12:31:10 +0200

    models commit

---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+from models.pcn import PCN
+from models.pcn_confidence import PCN_CONFIDENCE
+from models.pcn_confidence_2 import PCN_CONFIDENCE_2
---
+++ models/pcn_confidence_2.py
@@ -0,0 +1,164 @@
+import torch
+import torch.nn as nn
+
+
+class PCN_CONFIDENCE_2(nn.Module):
+    """
+    "PCN: Point Cloud Completion Network"
+    (https://arxiv.org/pdf/1808.00671.pdf)
+
+    Attributes:
+        num_dense:  16384
+        latent_dim: 1024
+        grid_size:  4
+        num_coarse: 1024
+    """
+
+    def __init__(self, num_dense=4096, latent_dim=1024, grid_size=2):
+        super().__init__()
+
+        self.num_dense = num_dense
+        self.latent_dim = latent_dim
+        self.grid_size = grid_size
+
+        assert self.num_dense % self.grid_size ** 2 == 0
+
+        self.num_coarse = self.num_dense // (self.grid_size ** 2)
+
+        self.first_conv = nn.Sequential(
+            nn.Conv1d(3, 128, 1),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(128, 256, 1)
+        )
+
+        self.second_conv = nn.Sequential(
+            nn.Conv1d(512, 512, 1),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(512, self.latent_dim, 1)
+        )
+
+        self.mlp = nn.Sequential(
+            nn.Linear(self.latent_dim, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, 3 * self.num_coarse)
+        )
+
+        self.final_conv = nn.Sequential(
+            nn.Conv1d(1024 + 3 + 2, 512, 1),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(512, 512, 1),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(512, 128, 1),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(128, 64, 1),
+            nn.BatchNorm1d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(64, 4, 1)  #changed 3 to 4 for confidence
+        )
+
+        '''
+        self.confidence_conv = nn.Sequential(
+            nn.Conv1d(1024 + 3 + 2, 512, 1),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(512, 128, 1),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(128, 32, 1),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(32, 1, 1),
+            #nn.Softmax(dim=1)
+            #nn.BatchNorm1d(1),
+            
+            #nn.ReLU(inplace=True)
+            #nn.Sigmoid()
+            #nn.BatchNorm1d(512),
+            #nn.ReLU(inplace=True)
+            #nn.Conv1d(512, 4, 1)  #changed 3 to 4 for confidence
+        )
+        '''
+        a = torch.linspace(-0.05, 0.05, steps=self.grid_size, dtype=torch.float).view(1, self.grid_size).expand(self.grid_size, self.grid_size).reshape(1, -1)
+        b = torch.linspace(-0.05, 0.05, steps=self.grid_size, dtype=torch.float).view(self.grid_size, 1).expand(self.grid_size, self.grid_size).reshape(1, -1)
+        
+        self.folding_seed = torch.cat([a, b], dim=0).view(1, 2, self.grid_size ** 2).cuda()  # (1, 2, S)
+
+
+    def forward(self, xyz):
+        B, N, _ = xyz.shape
+        
+        # encoder
+        feature = self.first_conv(xyz.transpose(2, 1))                                       # (B,  256, N)
+        feature_global = torch.max(feature, dim=2, keepdim=True)[0]                          # (B,  256, 1)
+        feature = torch.cat([feature_global.expand(-1, -1, N), feature], dim=1)              # (B,  512, N)
+        feature = self.second_conv(feature)                                                  # (B, 1024, N)
+        feature_global = torch.max(feature,dim=2,keepdim=False)[0]                           # (B, 1024)
+        
+        # decoder
+        coarse = self.mlp(feature_global).reshape(-1, self.num_coarse, 3)                    # (B, num_coarse, 3), coarse point cloud
+        point_feat = coarse.unsqueeze(2).expand(-1, -1, self.grid_size ** 2, -1)             # (B, num_coarse, S, 3)
+        point_feat = point_feat.reshape(-1, self.num_dense, 3).transpose(2, 1)               # (B, 3, num_fine)
+
+        seed = self.folding_seed.unsqueeze(2).expand(B, -1, self.num_coarse, -1)             # (B, 2, num_coarse, S)
+        seed = seed.reshape(B, -1, self.num_dense).to('cuda:1')                                           # (B, 2, num_fine)
+
+        feature_global = feature_global.unsqueeze(2).expand(-1, -1, self.num_dense)          # (B, 1024, num_fine)
+        feat = torch.cat([feature_global, seed, point_feat], dim=1)                          # (B, 1024+2+3, num_fine)
+    
+        fine_raw = self.final_conv(feat) #+ point_feat
+        #fine_raw = fine_raw.to('cuda:1') 
+        fine = fine_raw[:,:3,:] + point_feat
+        #fine = fine.to('cuda:1')
+        fine_t = fine.transpose(1, 2)
+        #fine_t = fine_t.to('cuda:1')
+        confidence = fine_raw[:,3,:]
+        print(confidence.size())
+        confidence = confidence.reshape(B,1,self.num_dense).transpose(1, 2)
+        #confidence = self.confidence_conv(feat).transpose(1,2)                                              # (B, 4, num_fine), fine point cloud
+        fine = torch.cat([fine_t, confidence], dim=2)
+        #fine = fine[:,:3,:]
+
+        return coarse.contiguous().to('cuda:1'), fine.contiguous().to('cuda:1'), confidence.squeeze().contiguous().to('cuda:1')
+
+'''
+    def forward(self, xyz):
+        B, N, _ = xyz.shape
+        
+        # encoder
+        feature = self.first_conv(xyz.transpose(2, 1))                                       # (B,  256, N)
+        feature_global = torch.max(feature, dim=2, keepdim=True)[0]                          # (B,  256, 1)
+        feature = torch.cat([feature_global.expand(-1, -1, N), feature], dim=1)              # (B,  512, N)
+        feature = self.second_conv(feature)                                                  # (B, 1024, N)
+        feature_global = torch.max(feature,dim=2,keepdim=False)[0]                           # (B, 1024)
+        
+        # decoder
+        coarse = self.mlp(feature_global).reshape(-1, self.num_coarse, 3)                    # (B, num_coarse, 3), coarse point cloud
+        point_feat = coarse.unsqueeze(2).expand(-1, -1, self.grid_size ** 2, -1)             # (B, num_coarse, S, 3)
+        point_feat = point_feat.reshape(-1, self.num_dense, 3).transpose(2, 1)               # (B, 3, num_fine)
+
+        seed = self.folding_seed.unsqueeze(2).expand(B, -1, self.num_coarse, -1)             # (B, 2, num_coarse, S)
+        seed = seed.reshape(B, -1, self.num_dense).to('cuda:1')                                           # (B, 2, num_fine)
+
+        feature_global = feature_global.unsqueeze(2).expand(-1, -1, self.num_dense)          # (B, 1024, num_fine)
+        feat = torch.cat([feature_global, seed, point_feat], dim=1)                          # (B, 1024+2+3, num_fine)
+    
+        fine = self.final_conv(feat) #+ point_feat 
+        fine = fine[:,:3,:] + point_feat
+        fine_t = fine.transpose(1, 2)
+        confidence = confidence[:,3,:]
+        confidence = confidence.transpose(1, 2)
+        #confidence = self.confidence_conv(feat).transpose(1,2)                                              # (B, 4, num_fine), fine point cloud
+        fine = torch.cat([fine_t, confidence], dim=2)
+        #fine = fine[:,:3,:]
+
+        return coarse.contiguous(), fine.contiguous(), confidence.squeeze().contiguous()
+        '''
+
+
+
